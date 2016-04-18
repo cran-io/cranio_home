ad13e0e7a4803757a07f6119bfe118e9381dfa7e
Delfina <delfinapolito@gmail.com>
2016-04-18 16:52:11 -0300

    contact form animation

--- js/home.js
+++ js/home.js
@@ -1,7 +1,10 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     $("#contact-button").click(function(){
+        $(".page-contact").addClass("contact-animation");
         $("#contact-form").removeClass("hidden");
         $("#contact-button").addClass("hidden");
+
+
     });
 });
