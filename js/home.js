e021fb3c46ec6c4737e94da50905b03c1cd730ff
Delfina <delfinapolito@gmail.com>
2016-04-22 16:54:11 -0300

    new changes in contact and navbar secions

--- js/home.js
+++ js/home.js
@@ -1,10 +1,39 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
     $("#contact-button").click(function(){
         $(".page-contact").addClass("contact-animation");
         $("#contact-form").removeClass("hidden");
         $("#contact-button").addClass("hidden");
-
+        $("#close-contact-button").removeClass("hidden");
 
     });
+    $("#close-contact-button").click(function(){
+      $(".page-contact").removeClass("contact-animation");
+      $("#contact-form").addClass("hidden");
+      $("#contact-button").removeClass("hidden");
+      $("#close-contact-button").addClass("hidden");
+
+    });
+
+    $(".navbar-toggle").click(function(){
+
+        var className = $('#responsive-navbar-button').attr('class');
+
+        if(className=='glyphicon glyphicon-menu-hamburger'){
+          $("#navbar-brand-container").fadeOut(0, function(){
+            $("#responsive-navbar-button").removeClass("glyphicon-menu-hamburger");
+            $(".navbar-toggle").attr("style", "background-image: url('./images/close.png')!important; background-size: 100% 100%!important; height:22px; background-repeat: no-repeat;");
+          });
+        }else{
+
+          $("#responsive-navbar-button").addClass("glyphicon-menu-hamburger");
+          $("#responsive-navbar-button").removeClass("glyphicon-remove-circle");
+          $("#navbar-brand-container").delay(500).fadeIn();
+          $(".navbar-toggle").attr("style", "background-image: none; background-size: inherit; height:auto; ");
+
+        }
+    });
+
+
+
+
 });
