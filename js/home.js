fe4df2c5451a2e067dd881366da88ef44705e021
Delfina <delfinapolito@gmail.com>
2016-06-07 16:48:48 -0300

    new team photos

--- js/home.js
+++ js/home.js
@@ -34,6 +34,124 @@ $( document ).ready(function() {
     });
 
 
+    $("#Juanvi").mouseover(function(){
+        $("#JuanviImg").attr('src', "images/team/juanvi1.jpg");
+    });
+    $("#Juanvi").mouseleave(function(){
+        $("#JuanviImg").attr('src', "images/team/juanvi.jpg");
+    });
+    $("#Jota").mouseover(function(){
+        $("#JotaImg").attr('src', "images/team/jota1.jpg");
+    });
+    $("#Jota").mouseleave(function(){
+        $("#JotaImg").attr('src', "images/team/jota.jpg");
+    });
+    $("#Sol").mouseover(function(){
+        $("#SolImg").attr('src', "images/team/sol1.jpg");
+    });
+    $("#Sol").mouseleave(function(){
+        $("#SolImg").attr('src', "images/team/sol.jpg");
+    });
+
+    $("#Santi").hover(
+      function () {
+        $("#SantiImg").attr('src', "images/team/Santi1.jpg");
+      },
+      function () {
+        $("#SantiImg").attr('src', "images/team/Santi.jpg");
+      }
+    );
+
+    $("#Pepe").hover(
+      function () {
+        $("#PepeImg").attr('src', "images/team/pepe1.jpg");
+      },
+      function () {
+        $("#PepeImg").attr('src', "images/team/pepe.jpg");
+      }
+    );
+
+    $("#Gaby").hover(
+      function () {
+        $("#GabyImg").attr('src', "images/team/gaby1.jpg");
+      },
+      function () {
+        $("#GabyImg").attr('src', "images/team/gaby.jpg");
+      }
+    );
+    $("#Javi").hover(
+      function () {
+        $("#JaviImg").attr('src', "images/team/javi1.jpg");
+      },
+      function () {
+        $("#JaviImg").attr('src', "images/team/javi.jpg");
+      }
+    );
+    $("#Chino").hover(
+      function () {
+        $("#ChinoImg").attr('src', "images/team/chino1.jpg");
+      },
+      function () {
+        $("#ChinoImg").attr('src', "images/team/chino.jpg");
+      }
+    );
+    $("#Delfi").hover(
+      function () {
+        $("#DelfiImg").attr('src', "images/team/delfi1.jpg");
+      },
+      function () {
+        $("#DelfiImg").attr('src', "images/team/delfi.jpg");
+      }
+    );
+    $("#Gon").hover(
+      function () {
+        $("#GonImg").attr('src', "images/team/gon1.jpg");
+      },
+      function () {
+        $("#GonImg").attr('src', "images/team/gon.jpg");
+      }
+    );
+
+    $("#Clau").hover(
+      function () {
+        $("#ClauImg").attr('src', "images/team/clau1.jpg");
+      },
+      function () {
+        $("#ClauImg").attr('src', "images/team/clau.jpg");
+      }
+    );
+    $("#Belu").hover(
+      function () {
+        $("#BeluImg").attr('src', "images/team/belu1.jpg");
+      },
+      function () {
+        $("#BeluImg").attr('src', "images/team/belu.jpg");
+      }
+    );
+    $("#Agus").hover(
+      function () {
+        $("#AgusImg").attr('src', "images/team/agus1.jpg");
+      },
+      function () {
+        $("#AgusImg").attr('src', "images/team/agus.jpg");
+      }
+    );
+    $("#Colo").hover(
+      function () {
+        $("#ColoImg").attr('src', "images/team/colo1.jpg");
+      },
+      function () {
+        $("#ColoImg").attr('src', "images/team/colo.jpg");
+      }
+    );
+    $("#Juli").hover(
+      function () {
+        $("#JuliImg").attr('src', "images/team/juli1.jpg");
+      },
+      function () {
+        $("#JuliImg").attr('src', "images/team/juli.jpg");
+      }
+    );
 
 
 });
